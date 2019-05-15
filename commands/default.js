53e0670ee18d4f0be0b77f4f8eb832f9999ab9df
dustinpfister <dustin.pfister@gmail.com>
2019-05-14 21:07:28 -0400

    updated default command

--- commands/default.js
+++ commands/default.js
@@ -2,5 +2,11 @@
 exports.command = '*';
 exports.describe = 'default command';
 exports.handler = function (argv) {
-  console.log('JSKEY: usage:');
+  console.log('> JSKEY: usage:');
+  console.log('> ');
+  console.log('> $ jskey init -t fooblog');
+  console.log('> creates a new project folder called fooblog in the current working dir');
+  console.log('> ');
+  console.log('> $ jskey edit -t fooblog -p 8080');
+  console.log('> starts editor for fooblog on port 8080');
 }
