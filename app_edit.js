478174d1dfb909e63f010223d83d8b92d0c3844f
dustinpfister <dustin.pfister@gmail.com>
2019-05-26 18:03:14 -0400

    started using ejs

--- app_edit.js
+++ app_edit.js
@@ -4,12 +4,25 @@ app = express();
 app.set('port', 8000);
 app.set('key', '1234');
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use('/js', express.static('./public/js'))
+
 app.get('/', (req, res) => {
-    let html =
-        '<h1>Editor:</h1>'+
-        '<p>key: ' + app.get('key') + '<\/p>';
-    res.send(html);
+    res.render('index', {
+        key: app.get('key')
+    });
 });
+
+/*
+app.get('/', (req, res) => {
+let html =
+'<h1>Editor:</h1>'+
+'<p>key: ' + app.get('key') + '<\/p>';
+res.send(html);
+});
+ */
 
 exports.listen = (opt) => {
     opt = opt || {};
