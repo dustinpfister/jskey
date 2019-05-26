b5b637191c47d7f85584a4395efcca002f8b1091
dustinpfister <dustin.pfister@gmail.com>
2019-05-26 18:32:08 -0400

    plugin list

--- middleware/plugin_loader.js
+++ middleware/plugin_loader.js
@@ -1,10 +1,29 @@
+let fs = require('fs');
+
+let getPluginList = (dir_plugins) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(dir_plugins, (e, plugins) => {
+            if (e) {
+                reject(e)
+            } else {
+                resolve(plugins);
+            }
+        });
+    });
+
+};
+
 module.exports = (opt) => {
 
     opt = opt || {};
     opt.dir_plugins = opt.dir_plugins || './plugins';
 
-	console.log(opt.dir_plugins);
-	
+    getPluginList(opt.dir_plugins).then((plugins) => {
+
+        console.log(plugins);
+
+    });
+
     // create a stack of actions from
     let actions = [
         (req, res) => {
