6960f9e0a47c9655d01f59a13f4b6167b8c6b0c3
dustinpfister <dustin.pfister@gmail.com>
2019-05-31 06:54:41 -0400

    click tab

--- public/js/client/vuejs_header.js
+++ public/js/client/vuejs_header.js
@@ -1,21 +1,31 @@
 new Vue({
     el: '#vue-header',
-    template: '<p><span v-for=\"pn in pluginNames\">{{ pn }}</span></p>',
+    template: '<div>' +
+    '<input type=\"button\" v-on:click=\"clickTab\" v-for=\"pn in pluginNames\" v-bind:value=\"pn\" >' +
+    '</div>',
     data: {
         pluginNames: ['none']
     },
+
+    // on created load plugin names
     created: function () {
-
         var vm = this;
-
         vm.$httpGet({
             url: '/plugin',
             onDone: function (res) {
-
                 vm.$data.pluginNames = res.pluginNames;
-
             }
         })
+    },
+
+    methods: {
+
+        // when a plugin tab is clicked
+        clickTab: function (e) {
+
+            console.log(e.target.value);
+
+        }
 
     }
 
